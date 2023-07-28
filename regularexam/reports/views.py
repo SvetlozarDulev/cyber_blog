8be8f35b59abd4c9a4c8dd5e7a8ac0d5e653e5d3
SvetlozarDulev <pr_hack@protonmail.com>
2023-07-28 16:04:35 +0300

    permissions and authorizations

--- regularexam/reports/views.py
+++ regularexam/reports/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from .models import Report
+
 
 # Create your views here.
 class ReportListView(ListView):
     template_name = 'report_list.html'
     model = Report
 
-class ReportEditView(UpdateView):
+
+class ReportEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Report
     template_name = 'report_edit.html'
     fields = (
@@ -16,21 +19,37 @@ class ReportEditView(UpdateView):
         'additional_materials',
     )
 
-class ReportDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ReportDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Report
     template_name = 'report_delete.html'
     success_url = reverse_lazy('report_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+
 class ReportDetailView(DetailView):
     model = Report
     template_name = 'report_detail.html'
 
-class ReportCreateView(CreateView):
+
+class ReportCreateView(LoginRequiredMixin, CreateView):
     model = Report
     template_name = 'report_create.html'
     fields = (
         'title',
         'body',
-        'author',
         'additional_materials',
         'common_vulnerability_scoring_system',
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
